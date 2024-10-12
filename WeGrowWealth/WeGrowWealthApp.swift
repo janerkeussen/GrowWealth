afe1977404a0d26796affcb64283e33f76da8975
janerkeussen <zhanerke.usen@gmail.com>
2024-10-12 11:35:58 -0700

    Initial Commit

---
+++ WeGrowWealth/WeGrowWealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeGrowWealthApp.swift
+//  WeGrowWealth
+//
+//  Created by Zhanerke Ussen on 12/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeGrowWealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
